40b5b74266f5640347d7b1c662233bd44925f5aa
waypse <slavapankov20@gmail.com>
2025-05-01 23:51:57 +0200

    fix: properly expose docker containers

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
--- svelte-kit/vite.config.ts
+++ svelte-kit/vite.config.ts
@@ -5,5 +5,14 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	optimizeDeps: {
 		exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+	},
+	mode: process.env.ENV === 'production' ? 'production' : 'development',
+	server: {
+		host: '0.0.0.0',
+		port: 5173,
+		strictPort: true,
+		hmr: {
+			clientPort: 5173
+		}
 	}
 });
